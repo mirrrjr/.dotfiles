ff4eb8b9df5e39ecc2fe71223cb7ed0406f8e4bf
mirrrjr <mirrrrjr@gmail.com>
2026-08-12 08:34:58 +0500

    add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "f",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+config.font_size = 12
+config.font = wezterm.font("JetBrains Mono", { weight = "Regular", italic = false })
+config.color_scheme = "Firefly Traditional"
+config.window_decorations = "NONE"
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+config.hide_mouse_cursor_when_typing = true
+config.default_cursor_style = "SteadyBlock"
+config.window_padding = {
+	left = 0,
+	top = 0,
+	right = 0,
+	bottom = 0,
+}
+
+config.line_height = 0.90
+config.cell_width = 1.0
+
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+
+config.underline_position = -2
+
+-- Finally, return the configuration to wezterm:
+return config
